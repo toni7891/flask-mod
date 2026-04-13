49acd6073f1e40587f3b35e10280a1723e19c59e
toni7891 <toniv7891@gmail.com>
2026-04-13 16:26:36 +0300

    .

--- lab2.py
+++ lab2.py
@@ -51,7 +51,32 @@ def echo():
         "echo" : body
     }), 200
 
+USERS = [
+    {"id": 1 , "name": "bob"},
+    {"id": 2 , "name": "gam"},
+    {"id": 3 , "name": "bob bam"},
+    {"id": 4 , "name": "bim"}
+]
 
+@app.route('/search')
+def search():
+    name = request.args.get("name")
+    
+    if not name:
+        return jsonify({
+            "success" : False,
+            "error": "name required"
+        })
+        
+    res = []
+    for u in USERS:
+        if name in u["name"]:
+            res.append(u)
+            
+    return jsonify({
+        "success" : True,
+        "name" : res                
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
