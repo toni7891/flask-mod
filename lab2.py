e4aa7288d7942587052043729f5508e73366a16a
toni7891 <toniv7891@gmail.com>
2026-04-13 16:08:49 +0300

    LAB2 PRAC

--- lab2.py
+++ lab2.py
@@ -1,8 +1,57 @@
-def main():
+from flask import Flask, jsonify, request
+from datetime import datetime, timezone 
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "API is running"
+    })
+    
+
+@app.route('/status')
+def status():
+    return jsonify({
+        "status" : "ok",
+        "version" : "1.0.0"
+    })
     
     
+@app.route('/time')
+def cur_time():
+    now = datetime.now(timezone.utc)
     
-    pass
+    return jsonify({
+        "time" : now
+    }) 
+
+
+@app.route('/info')
+def info():    
+    return jsonify({
+        "app" : "Flask prac",
+        "author" : "Tony",
+        "day": 2
+    })
+    
+@app.route('/echo', methods=["POST"])
+def echo():
+    body = request.json 
+    
+    if not bool(body):
+        return jsonify({
+            "success" : False,
+            "error" : "body not found"
+            
+        }), 400
+    
+    return jsonify({
+        "success" : True,
+        "echo" : body
+    }), 200
+
+
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
